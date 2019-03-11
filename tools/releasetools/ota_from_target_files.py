d2619f7b39cde1626fb48d186e6457480780e569
lindwurm <lindwurm.q@gmail.com>
2019-03-11 17:37:17 +0900

    Floko: Initialize our logo
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -823,19 +823,13 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("Target: {}".format(target_info.fingerprint))
 
   # Build info
-  script.Print("                                         ");
-  script.Print("            _____            _     _     ");
-  script.Print("           |  __ \          (_)   | |    ");
-  script.Print("   ___ _ __| |  | |_ __ ___  _  __| |    ");
-  script.Print("  / __| '__| |  | | '__/ _ \| |/ _\`|    ");
-  script.Print(" | (__| |  | |__| | | | (_) | | (_| |    ");
-  script.Print("  \___|_|  |_____/|_|_ \___/|_|\__,_|  _ ");
-  script.Print("     /\             | |         (_)   | |");
-  script.Print("    /  \   _ __   __| |_ __ ___  _  __| |");
-  script.Print("   / /\ \ | '_ \ / _\`| '__/ _ \| |/ _\`|");
-  script.Print("  / ____ \| | | | (_| | | | (_) | | (_| |");
-  script.Print(" /_/    \_\_| |_|\__,_|_|  \___/|_|\__,_|");
-  script.Print("                                         ");
+  script.Print("    ______ _       _           ");
+  script.Print("   |  ____| |     | |          ");
+  script.Print("   | |__  | | ___ | | _____    ");
+  script.Print("   |  __| | |/ _ \| |/ / _ \   ");
+  script.Print("   | |    | | (_) |   < (_) |  ");
+  script.Print("   |_|    |_|\___/|_|\_\___/   ");
+  script.Print("                               ");
 
   buildid = target_info.GetBuildProp("ro.modversion")
   androidver = target_info.GetBuildProp("ro.build.version.release")
